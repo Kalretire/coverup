b201540f7f56493802ff7fc15422a0052b1430d9
horry10 <horry10@naver.com>
2023-10-26 14:16:42 +0900

    qwfhfwehui

--- src/main/java/com/last/application/controller/startcontroller.java
+++ src/main/java/com/last/application/controller/startcontroller.java
@@ -21,4 +21,46 @@ public class startcontroller {
         model.setViewName("login");
         return model;
     }
+
+    @GetMapping("AIrecommand")
+    public ModelAndView AIrecommand(ModelAndView model){
+        System.out.println("AIrecommand");
+        model.setViewName("AIrecommand");
+        return model;
+    }
+    @GetMapping("community")
+    public ModelAndView community(ModelAndView model){
+        System.out.println("community");
+        model.setViewName("community");
+        return model;
+    }
+    @GetMapping("productRating")
+    public ModelAndView productRating(ModelAndView model){
+        System.out.println("productRating");
+        model.setViewName("productRating");
+        return model;
+    }
+
+    @GetMapping("productRecommand")
+    public ModelAndView productRecommand(ModelAndView model){
+        System.out.println("productRecommand");
+        model.setViewName("productRecommand");
+        return model;
+    }
+
+    @GetMapping("mypage")
+    public ModelAndView mypage(ModelAndView model){
+        System.out.println("mypage");
+        model.setViewName("mypage");
+        return model;
+    }
+
+    @GetMapping("flask")
+    public ModelAndView flask(ModelAndView model){
+        System.out.println("flask");
+        model.setViewName("flask");
+        return model;
+    }
+
+
 }
